0bebc25e9cda68d36adf1bebe8702f7377d2d681
Jadzia-Serfoss <123050136+Jadzia-Serfoss@users.noreply.github.com>
2025-04-07 16:37:26 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const playerDisplay = document.querySelector('.player-display');
+const computerDisplay = document.querySelector('.computer-display');
+const resultDisplay = document.querySelector('.result');
+const playerScoreDisplay = document.querySelector('.player-score');
+const computerScoreDisplay = document.querySelector('.computer-score');
+
+let playerScore = '0';
+let computerScore = '0';
+
+const choices = ['Rock', 'Paper', 'Scissors'];
+
+function game(playerChoice) {
+  const computerChoice = choices[Math.floor(Math.random() * 3)];
+
+  const getResult = (playerChoice, computerChoice) => 
+
+  playerChoice === computerChoice ? "It's a tie!" :
+  (playerChoice === 'Rock' && computerChoice === 'Scissors') || 
+  (playerChoice === 'Paper' && computerChoice === 'Rock') || 
+  (playerChoice === 'Scissors' && computerChoice === 'Paper') 
+  ? "YOU WIN!" : "You lose...";
+
+  const result = getResult(playerChoice, computerChoice);
+  playerDisplay.textContent = `Player: ${playerChoice}`;
+  computerDisplay.textContent = `Computer: ${computerChoice}`;
+  resultDisplay.textContent = result;
+
+  if (result === "YOU WIN!") {
+    playerScore++;
+  } else if (result === "You lose...") {
+    computerScore++;
+  };
+
+  playerScoreDisplay.textContent = `${playerScore}`;
+  computerScoreDisplay.textContent = `${computerScore}`;
+}
